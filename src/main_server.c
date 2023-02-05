7ef6c1e1ad4b37c61d0a792f96ac70a2c2525929
Aytixel <aytixel@gmail.com>
2023-02-05 21:39:46 +0100

    Modification de main_server.c
    Ajout de la fonction get_connection_info qui récupère les informations pour le serveur dans les arguments du main.
    Ajout de l'initialisation du serveur socket.
    Ajout d'écouteur d'évènement pour terminé le programme proprement.

--- src/main_server.c
+++ src/main_server.c
@@ -1,25 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
 #include "timer.h"
 #include "server_socket.h"
 
-int main()
+int running = 1;
+
+void handler(int s)
 {
-  initSocket();
+    running = 0;
+}
 
-  frame_timer_t *main_timer = createTimer(1000 / 60);
-
-  while (1)
-  {
-    if (checkTime(main_timer))
+int get_connection_info(int argc, char *argv[], char **hostname, uint16_t *port)
+{
+    for (int i = 1; i < argc; i++)
     {
-      printf("Serveur\n");
-
-      sleepMs(timeLeft(main_timer));
+        if (*hostname == NULL && (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--hostname") == 0) && ++i < argc)
+        {
+            *hostname = malloc(sizeof(char) * (strlen(argv[i]) + 1));
+            strcpy(*hostname, argv[i]);
+        }
+        else if (*port == 0 && (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--port") == 0) && ++i < argc)
+        {
+            *port = atoi(argv[i]);
+        }
     }
-  }
 
-  deleteTimer(&main_timer);
-  endSocket();
+    if (*hostname == NULL)
+    {
+        return -1;
+    }
+    if (*port == 0)
+    {
+        free(*hostname);
 
-  return 0;
+        return -1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    char *hostname = NULL;
+    uint16_t port = 0;
+
+    if (get_connection_info(argc, argv, &hostname, &port) == -1)
+    {
+        printf("Erreur : Nom d'hôte ou port non spécifié.\n");
+
+        return 1;
+    }
+
+    printf("Ecoute sur %s:%d\n", hostname, port);
+
+    signal(SIGINT, handler);
+    signal(SIGABRT, handler);
+    signal(SIGTERM, handler);
+
+    initSocket();
+
+    server_t *server = createServer(hostname, port);
+
+    if (server == NULL)
+    {
+        endSocket();
+        free(hostname);
+
+        return 2;
+    }
+
+    frame_timer_t *main_timer = createTimer(1000 / 60);
+
+    // boucle principale
+    while (running)
+    {
+        if (checkTime(main_timer))
+        {
+            sleepMs(timeLeft(main_timer));
+        }
+    }
+
+    deleteTimer(&main_timer);
+    deleteServer(&server);
+    endSocket();
+    free(hostname);
+
+    return 0;
 }
