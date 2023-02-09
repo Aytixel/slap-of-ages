ac1268fcbb9d634071a76ed6702233fad84e2d9c
ABTonniere <hoa.leluet@gmail.com>
2023-02-09 11:56:53 +0100

    Suppression fonctions inutiles

--- src/test_animation.c
+++ src/test_animation.c
@@ -6,97 +6,10 @@
 #include <SDL2/SDL_ttf.h>
 #include "timer.h"
 #include "test_animation.h"
+#include "window.h"
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
-
-// Fonction de gestion des évènements
-void handleEvent(SDL_Event *event)
-{
-  switch (event->type)
-  {
-  // Si l'utilisateur ferme la fenêtre
-  case SDL_QUIT:
-    exit(0);
-    break;
-  }
-}
-
-int initialisationSDL()
-{
-  // Initialisation de la SDL
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
-  {
-    fprintf(stderr, "Erreur lors de l'initialisation de la SDL : %s", SDL_GetError());
-    return 1;
-  }
-
-  return 0;
-}
-
-SDL_Window *creationFenetre()
-{
-  // Création de la fenêtre
-  SDL_Window *window = SDL_CreateWindow("Tests Animations", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-  if (window == NULL)
-  {
-    fprintf(stderr, "Erreur lors de la création de la fenêtre : %s", SDL_GetError());
-    SDL_Quit();
-    return NULL;
-  }
-  return window;
-}
-
-SDL_Renderer *creationRenderer(SDL_Window *window)
-{
-  // Création du renderer
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if (renderer == NULL)
-  {
-    fprintf(stderr, "Erreur lors de la création du renderer : %s", SDL_GetError());
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return NULL;
-  }
-  return renderer;
-}
-
-void renderDrawColor(SDL_Renderer *renderer)
-{
-  // Définition de la couleur de fond
-  SDL_SetRenderDrawColor(renderer, 100, 10, 0, 255);
-}
-
-int checkTTFLib(SDL_Window *window, SDL_Renderer *renderer)
-{
-  // Check de la librairie
-  if (TTF_Init() == -1)
-  {
-    fprintf(stderr, "Erreur lors de l'initialisation de la SDL_ttf : %s", TTF_GetError());
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 1;
-  }
-
-  return 0;
-}
-
-TTF_Font *loadFont(SDL_Window *window, SDL_Renderer *renderer)
-{
-  // Chargement de la police
-  TTF_Font *font = TTF_OpenFont("asset/font/arial.ttf", 24);
-  if (font == NULL)
-  {
-    fprintf(stderr, "Erreur lors du chargement de la police : %s", TTF_GetError());
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return NULL;
-  }
-
-  return font;
-}
 
 int initFrames(SDL_Rect *tab, int nb_frames, SDL_Surface *src, int line, int nb_lines)
 {
@@ -159,7 +72,7 @@ int destroyAnim(anim_t **anim)
   return 0;
 }
 
-void updateAnim(anim_t *anim, portal_e current_state, SDL_Renderer *renderer)
+void updateAnim(anim_t *anim, portal_e current_state, window_t *window)
 {
 
   anim->current_frame++;
@@ -172,7 +85,7 @@ void updateAnim(anim_t *anim, portal_e current_state, SDL_Renderer *renderer)
   if (current_state == ANIMATION_DELETE)
   {
     destroyAnim(&anim);
-    SDL_RenderClear(renderer);
+    SDL_RenderClear(window->renderer);
     return;
   }
 
@@ -181,19 +94,15 @@ void updateAnim(anim_t *anim, portal_e current_state, SDL_Renderer *renderer)
     anim->current_frame = 0;
   }
 
-  SDL_RenderCopy(renderer, anim->sprite, &anim->anims[anim->current_state][anim->current_frame], anim->size);
-  
+  SDL_RenderCopy(window->renderer, anim->sprite, &anim->anims[anim->current_state][anim->current_frame], anim->size);
 }
 
 int main(int argc, char *argv[])
 {
 
-  initialisationSDL();
+  window_t *window = createWindow("Test Animations", WINDOW_WIDTH, WINDOW_HEIGHT);
 
-  SDL_Window *window = creationFenetre();
-  SDL_Renderer *renderer = creationRenderer(window);
-
-  SDL_Surface *dim;
+  SDL_Surface *dim = IMG_Load("asset/GreenPortal.png");
 
   SDL_Rect portal_size = {0, 0, 150, 150};
   int states[] = {8, 8, 6};
@@ -202,15 +111,16 @@ int main(int argc, char *argv[])
       8,
       states,
       3,
-      SDL_CreateTextureFromSurface(renderer, dim = IMG_Load("asset/GreenPortal.png")),
+      SDL_CreateTextureFromSurface(window->renderer, dim),
       dim,
       &portal_size);
 
+  dim = IMG_Load("asset/PurplePortal.png");
   anim_t *purple_portal = createAnim(
       8,
       states,
       3,
-      SDL_CreateTextureFromSurface(renderer, dim = IMG_Load("asset/PurplePortal.png")),
+      SDL_CreateTextureFromSurface(window->renderer, dim),
       dim,
       &portal_size);
 
@@ -220,53 +130,50 @@ int main(int argc, char *argv[])
   {
     SDL_Event e;
 
+    if (SDL_PollEvent(&e))
+    {
+      if (e.type == SDL_QUIT)
+      {
+        break;
+      }
+    }
+
+    // Début boucle animations
+
     if (checkTime(main_timer))
     {
-      SDL_RenderClear(renderer);
+      SDL_RenderClear(window->renderer);
 
       if (SDL_PollEvent(&e))
       {
-        if (e.type == SDL_QUIT)
-        {
-          break;
-        }
-        else if (e.type == SDL_MOUSEBUTTONDOWN)
+        if (e.type == SDL_MOUSEBUTTONDOWN)
         {
 
           if (e.button.clicks == 1 && (e.button.button == SDL_BUTTON_LEFT || e.button.button == SDL_BUTTON_RIGHT))
           {
             if (e.button.button == SDL_BUTTON_LEFT)
             {
-              green_portal->size->x = e.button.x - (green_portal->size->w/2);
-              green_portal->size->y = e.button.y - (green_portal->size->h/2);
-              updateAnim(green_portal, ANIMATION_IDLE, renderer);
+              green_portal->size->x = e.button.x - (green_portal->size->w / 2);
+              green_portal->size->y = e.button.y - (green_portal->size->h / 2);
+              updateAnim(green_portal, ANIMATION_IDLE, window);
             }
 
             else
             {
-              purple_portal->size->x = e.button.x - (purple_portal->size->w/2);
-              purple_portal->size->y = e.button.y - (purple_portal->size->h/2);
-              updateAnim(purple_portal, ANIMATION_SPAWN, renderer);
+              purple_portal->size->x = e.button.x - (purple_portal->size->w / 2);
+              purple_portal->size->y = e.button.y - (purple_portal->size->h / 2);
+              updateAnim(purple_portal, ANIMATION_SPAWN, window);
             }
           }
         }
       }
-      updateAnim(green_portal, ANIMATION_CURRENT, renderer);
-      updateAnim(purple_portal, ANIMATION_CURRENT, renderer);
-      SDL_RenderPresent(renderer);
-      SDL_Delay(timeLeft(main_timer));
-
-      // Effacement de l'écran
-      // SDL_RenderClear(renderer);
-
-      // Mise à jour de l'affichage
-      // SDL_RenderPresent(renderer);
+      updateAnim(green_portal, ANIMATION_CURRENT, window);
+      updateAnim(purple_portal, ANIMATION_CURRENT, window);
+      SDL_RenderPresent(window->renderer);
     }
   }
 
-  // TTF_CloseFont(font);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+  destroyWindow(&window);
   TTF_Quit();
   SDL_Quit();
 
