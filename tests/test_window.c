9fcd17c5678665a194cfda063a52f97f3f1bea72
Aytixel <aytixel@gmail.com>
2023-02-12 15:40:48 +0100

    Ajout d'une boucle d’événement à test_window.c

--- tests/test_window.c
+++ tests/test_window.c
@@ -3,6 +3,19 @@
 #include "timer.h"
 #include "window.h"
 
+int running = 1;
+
+void windowEventHandler(SDL_Event *event)
+{
+    // gestion des évènements de la fenêtre
+    switch (event->type)
+    {
+    case SDL_QUIT:
+        running = 0;
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     printf("Test de window.h\n\n");
@@ -12,7 +25,26 @@ int main(int argc, char *argv[])
     assert(window != NULL);
     printf("Création du window OK\n");
 
-    sleepMs(3000);
+    frame_timer_t *main_timer = createTimer(1000 / 60);
+
+    // boucle principale
+    while (running)
+    {
+        SDL_Event event;
+        int time_left = timeLeft(main_timer);
+
+        if (SDL_WaitEventTimeout(&event, time_left > 0 ? time_left : 0))
+            windowEventHandler(&event);
+
+        if (checkTime(main_timer))
+        {
+            SDL_RenderClear(window->renderer);
+
+            SDL_RenderPresent(window->renderer);
+        }
+    }
+
+    deleteTimer(&main_timer);
 
     assert(!destroyWindow(&window));
     assert(window == NULL);
